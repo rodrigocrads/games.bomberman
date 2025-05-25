3284141a58e45a9a7d5927d006e35666a4ec8906
Rodrigo Cunha Rodrigues <wkm.rcunha@terceiros.mag.com.br>
2025-05-25 18:47:05 -0300

    refactor: rename TestScene to BattleScene

---
+++ src/game/scenes/BattleScene.js
@@ -0,0 +1,19 @@
+import { Scene } from 'engine/Scene.js';
+
+export class BattleScene extends Scene {
+  constructor() {
+    super();
+  }
+
+  update(time, context, camera) {
+    // Add your main update calls here
+  }
+
+  draw(context, camera) {
+    // Add your main draw calls here
+  }
+
+  cleanUp() {
+    // Can be used to clean
+  }
+}
