83447f8f6b069f20414f8ffd56b9c79dde990a3e
Rodrigo Cunha Rodrigues <wkm.rcunha@terceiros.mag.com.br>
2025-05-25 18:46:44 -0300

    refactor: rename TemplateExample to BombermanGame

--- src/game/BombermanGame.js
+++ src/game/BombermanGame.js
@@ -2,7 +2,7 @@ import { SCREEN_HEIGHT, SCREEN_WIDTH } from 'game/constants/game.js';
 import { TestScene } from 'game/scenes/TestScene.js';
 import { Game } from 'engine/Game.js';
 
-export class TemplateExample extends Game {
+export class BombermanGame extends Game {
 	scene = new TestScene(this.frameTime, this.camera);
 
 	constructor() {
