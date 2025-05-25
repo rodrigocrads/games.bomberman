cec7f0409d6f5bda1fd0d5822a4aca5860379fe4
Rodrigo Cunha Rodrigues <wkm.rcunha@terceiros.mag.com.br>
2025-05-25 19:06:34 -0300

    feat: create LevelData and LevelMap

---
+++ src/game/entities/LevelMap.js
@@ -0,0 +1,15 @@
+import { Scene } from 'engine/Scene.js';
+
+export class LevelMap extends Scene {
+  constructor() {
+    super();
+  }
+
+  update(time, context, camera) {
+    // Add your main update calls here
+  }
+
+  draw(context, camera) {
+    // Add your main draw calls here
+  }
+}
