31b5142361fd454f8e43611d9890efcdcfe375c4
Rodrigo Cunha Rodrigues <wkm.rcunha@terceiros.mag.com.br>
2025-05-27 21:05:41 -0300

    feat: add moving command

--- src/game/entities/Bomberman.js
+++ src/game/entities/Bomberman.js
@@ -1,13 +1,16 @@
 import { drawFrameOrigin } from 'engine/context.js';
 import { Entity } from 'engine/Entity.js';
-import { animations, BombermanStateType, frames } from 'game/constants/bomberman.js';
+import * as control from 'engine/inputHandler.js';
+import { animations, BombermanStateType, frames, WALK_SPEED } from 'game/constants/bomberman.js';
 import { Direction } from 'game/constants/entities.js';
 import { FRAME_TIME, HALF_TILE_SIZE, TILE_SIZE } from 'game/constants/game.js';
+import { isZero } from 'game/utils/utils.js';
 
 export class Bomberman extends Entity {
   image = document.querySelector('img#bomberman');
+  id = 0;
   direction = Direction.DOWN;
-  baseSpeedTime = 12;
+  baseSpeedTime = 1.2;
   speedMultiplier = 1;
   animation = animations.moveAnimations[this.direction];
 
@@ -19,11 +22,30 @@ export class Bomberman extends Entity {
         type: BombermanStateType.IDLE,
         init: this.handleIdleInit,
         update: this.handleIdleState,
+      },
+      [BombermanStateType.MOVING]: {
+        type: BombermanStateType.MOVING,
+        init: this.handleMovingInit,
+        update: this.handleMovingState,
       }
     }
 
     this.changeState(BombermanStateType.IDLE, time);
   }
+
+  getMovement() {
+    if (control.isLeft(this.id)) {
+      return [Direction.LEFT, { x: -WALK_SPEED, y: 0 }];
+    } else if (control.isRight(this.id)) {
+      return [Direction.RIGHT, { x: WALK_SPEED, y: 0 }];
+    } else if (control.isUp(this.id)) {
+      return [Direction.UP, { x: 0, y: -WALK_SPEED }];
+    } else if (control.isDown(this.id)) {
+      return [Direction.DOWN, { x: 0, y: WALK_SPEED }];
+    }
+
+    return [this.direction, { x: 0, y: 0 }];
+  };
 
   changeState(newState, time) {
     this.currentState = this.states[newState];
@@ -37,10 +59,48 @@ export class Bomberman extends Entity {
     this.velocity = { x: 0, y: 0 };
   };
 
-  handleIdleState = () => { };
+  handleMovingInit = () => {
+    this.animationFrame = 1;
+  };
+
+  handleGeneralState = () => {
+    const [direction, velocity] = this.getMovement();
+    this.animation = animations.moveAnimations[direction];
+    this.direction = direction;
+
+    return velocity;
+  };
+
+  handleIdleState = (time) => {
+    const velocity = this.handleGeneralState();
+    if (isZero(velocity)) return;
+    this.changeState(BombermanStateType.MOVING, time);
+  };
+
+  handleMovingState = (time) => {
+    this.velocity = this.handleGeneralState();
+    if (!isZero(this.velocity)) return;
+    this.changeState(BombermanStateType.IDLE, time);
+  };
+
+  updatePosition(time) {
+    this.position.x += (this.velocity.x * this.baseSpeedTime * this.speedMultiplier) * time.secondsPassed;
+    this.position.y += (this.velocity.y * this.baseSpeedTime * this.speedMultiplier) * time.secondsPassed;
+  }
+
+  updateAnimation(time) {
+    if (time.previous < this.animationTimer) return;
+    this.animationFrame += 1;
+    if (this.animationFrame >= this.animation.length) {
+      this.animationFrame = 0;
+    }
+    this.animationTimer = time.previous + (this.animation[this.animationFrame][1] * FRAME_TIME);
+  }
 
   update(time) {
+    this.updatePosition(time);
     this.currentState.update(time);
+    this.updateAnimation(time);
   }
 
   draw(context, camera) {
